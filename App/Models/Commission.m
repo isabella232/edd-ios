ff343528565b9e26ddc3ac21ca97162c473856d2
Matthew Strickland <matthew@idlefusion.com>
2014-02-24 15:56:30 -0500

    Commission Model

---
+++ App/Models/Commission.m
@@ -0,0 +1,24 @@
+//
+//  Commission.m
+//  EDDSalesTracker
+//
+//  Created by Matthew Strickland on 2/24/14.
+//  Copyright (c) 2014 Easy Digital Downloads. All rights reserved.
+//
+
+#import "Commission.h"
+
+@implementation Commission
+
+- (id)initWithAttributes:(NSDictionary *)attributes {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+	
+	
+	
+	return self;
+}
+
+@end
