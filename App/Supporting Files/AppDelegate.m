1a6a70c3498c2d9f067413e4c0a41862dc54260d
Matthew Strickland <matthew@idlefusion.com>
2014-10-15 12:15:25 -0400

    Status bar color #35

--- App/Supporting Files/AppDelegate.m
+++ App/Supporting Files/AppDelegate.m
@@ -24,7 +24,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {	
 //	NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
-//	[NSURLCache setSharedURLCache:URLCache];
+    //	[NSURLCache setSharedURLCache:URLCache];
+    
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
 	
 	[[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
     
