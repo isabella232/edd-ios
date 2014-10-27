9610bd65dcba2803284a3a489aa6499004e386be
Matthew Strickland <matthew@idlefusion.com>
2014-10-27 16:51:09 -0400

    fix menu layout

--- App/Controllers/EDDMenuViewController.m
+++ App/Controllers/EDDMenuViewController.m
@@ -29,7 +29,7 @@
 
 enum {
     MenuHomeRow = 0,
-	MenuEarningsRow,
+    MenuEarningsRow,
     MenuProductsRow,
     MenuSalesRow,
     MenuCustomersRow,
@@ -49,26 +49,26 @@ enum {
     if (!self) {
         return nil;
     }
-	
-	return self;
+    
+    return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	
+    
     self.tableView.backgroundColor = [UIColor colorWithHexString:@"#eeeeee"];
-	
+    
     [self.tableView registerNib:[self menuCellNib] forCellReuseIdentifier:@"MenuCell"];
     
-	[self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     
     self.tableView.scrollEnabled = [EDDHelpers isHandset];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-	[super viewWillAppear:animated];
+    [super viewWillAppear:animated];
     
-	[self.tableView reloadData];
+    [self.tableView reloadData];
 }
 
 - (void)refreshMenu {
@@ -82,7 +82,7 @@ enum {
 }
 
 - (BOOL)shouldAutorotate {
-	return NO;
+    return NO;
 }
 
 #pragma mark - Table view data source
@@ -92,52 +92,52 @@ enum {
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-	if (section == 0) {
-		if ([EDDSettingsHelper isCommissionOnlySite]) {
-			return 3;
-		} else if ([EDDSettingsHelper isStandardAndCommissionSite] || [EDDSettingsHelper isStandardAndStoreCommissionSite]) {
-			return MenuRowCount + 1;
-		} else {
-			return MenuRowCount;
-		}
-	}
-	return [self siteCount];
+    if (section == 0) {
+        if ([EDDSettingsHelper isCommissionOnlySite]) {
+            return 3;
+        } else if ([EDDSettingsHelper isStandardAndCommissionSite] || [EDDSettingsHelper isStandardAndStoreCommissionSite]) {
+            return MenuRowCount + 1;
+        } else {
+            return MenuRowCount;
+        }
+    }
+    return [self siteCount];
 }
 
 - (void)configureCell:(MenuCell *)cell forIndexPath:(NSIndexPath *)indexPath {
-	if (indexPath.section == 0) {
-		[self setupMenuCell:cell forIndexPath:indexPath];
-	} else {
-		NSDictionary *site = [self getSite:indexPath.row];
-		
-		cell.siteID = [site objectForKey:KEY_FOR_SITE_ID];
-		cell.label.text = [site objectForKey:KEY_FOR_SITE_NAME];
-	}
+    if (indexPath.section == 0) {
+        [self setupMenuCell:cell forIndexPath:indexPath];
+    } else {
+        NSDictionary *site = [self getSite:indexPath.row];
+        
+        cell.siteID = [site objectForKey:KEY_FOR_SITE_ID];
+        cell.label.text = [site objectForKey:KEY_FOR_SITE_NAME];
+    }
 }
 
 - (void)setupMenuCell:(MenuCell *)cell forIndexPath:(NSIndexPath *)indexPath {
-	if ([EDDSettingsHelper isCommissionOnlySite]) {
-		switch (indexPath.row) {
-			case 0:
-				cell.label.text = @"Home";
-				break;
-			case 1:
-				cell.label.text = @"Commissions";
-				break;
-			case 2:
-				cell.label.text = @"Setup";
-				break;
-		}
-	} else if ([EDDSettingsHelper isStandardAndCommissionSite] || [EDDSettingsHelper isStandardAndStoreCommissionSite]) {
-		switch (indexPath.row) {
-			case 0:
-				cell.label.text = @"Home";
-				break;
-			case 1:
-				cell.label.text = @"Earnings";
-				break;
-			case 2:
-				cell.label.text = @"Products";
+    if ([EDDSettingsHelper isCommissionOnlySite]) {
+        switch (indexPath.row) {
+            case 0:
+                cell.label.text = @"Home";
+                break;
+            case 1:
+                cell.label.text = @"Commissions";
+                break;
+            case 2:
+                cell.label.text = @"Setup";
+                break;
+        }
+    } else if ([EDDSettingsHelper isStandardAndCommissionSite] || [EDDSettingsHelper isStandardAndStoreCommissionSite]) {
+        switch (indexPath.row) {
+            case 0:
+                cell.label.text = @"Home";
+                break;
+            case 1:
+                cell.label.text = @"Earnings";
+                break;
+            case 2:
+                cell.label.text = @"Products";
                 break;
             case 3:
                 cell.label.text = @"Sales";
@@ -145,26 +145,26 @@ enum {
             case 4:
                 cell.label.text = @"Customers";
                 break;
-			case 5:
-				cell.label.text = @"Commissions";
-				break;
-			case 6:
-				cell.label.text = @"About";
-				break;
-			case 7:
-				cell.label.text = @"Setup";
-				break;
-		}
-	} else {
-		switch (indexPath.row) {
-			case 0:
-				cell.label.text = @"Home";
-				break;
-			case 1:
-				cell.label.text = @"Earnings";
-				break;
-			case 2:
-				cell.label.text = @"Products";
+            case 5:
+                cell.label.text = @"Commissions";
+                break;
+            case 6:
+                cell.label.text = @"About";
+                break;
+            case 7:
+                cell.label.text = @"Setup";
+                break;
+        }
+    } else {
+        switch (indexPath.row) {
+            case 0:
+                cell.label.text = @"Home";
+                break;
+            case 1:
+                cell.label.text = @"Earnings";
+                break;
+            case 2:
+                cell.label.text = @"Products";
                 break;
             case 3:
                 cell.label.text = @"Sales";
@@ -172,17 +172,17 @@ enum {
             case 4:
                 cell.label.text = @"Customers";
                 break;
-			case 5:
-				cell.label.text = @"About";
-				break;
-			case 6:
-				cell.label.text = @"Setup";
-				break;
-		}
-	}
-	
-	cell.labelCheckmark.text = @"";
-	cell.labelCheckmark.hidden = YES;
+            case 5:
+                cell.label.text = @"About";
+                break;
+            case 6:
+                cell.label.text = @"Setup";
+                break;
+        }
+    }
+    
+    cell.labelCheckmark.text = @"";
+    cell.labelCheckmark.hidden = YES;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -195,14 +195,14 @@ enum {
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section	{
-	switch (section) {
-		case 0:
-			return @"EASY DIGITAL DOWNLOADS";
-		case 1:
-			return NSLocalizedString(@"SITES", nil);
-		default:
-			return nil;
-	}
+    switch (section) {
+        case 0:
+            return @"EASY DIGITAL DOWNLOADS";
+        case 1:
+            return NSLocalizedString(@"SITES", nil);
+        default:
+            return nil;
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
@@ -214,7 +214,7 @@ enum {
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
     NSString *title = [self tableView:tableView titleForHeaderInSection:section];
     if (title == nil) return nil;
-	
+    
     return [EDDHelpers isHandset] || section != 0 ? [self headerViewForHandset:section] : [self headerViewForTablet:section];
 }
 
@@ -226,7 +226,9 @@ enum {
     label.font = [UIFont boldSystemFontOfSize:13];
     label.text = [self tableView:self.tableView titleForHeaderInSection:section];
     
-    SAMGradientView *gradient = [[SAMGradientView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 26.0f)];
+    CGFloat yPosition = section == 0 ? 20.0f : 0;
+    
+    SAMGradientView *gradient = [[SAMGradientView alloc] initWithFrame:CGRectMake(0, yPosition, self.view.frame.size.width, 26.0f)];
     gradient.backgroundColor = [UIColor clearColor];
     
     UIColor *first = [UIColor colorWithHexString:@"#ffffff"];
@@ -239,7 +241,19 @@ enum {
     
     [gradient addSubview:label];
     
-    return gradient;
+    if (section == 0) {
+        
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 46.0f)];
+        
+        view.backgroundColor = [UIColor colorWithHexString:@"#1c5585"];
+        
+        [view addSubview:gradient];
+        
+        return view;
+        
+    } else {
+        return gradient;
+    }
 }
 
 - (UIView *)headerViewForTablet:(NSInteger)section  {
@@ -261,7 +275,7 @@ enum {
 }
 
 - (CGFloat)headerHeightForSection:(NSInteger)section {
-    CGFloat height = 26.0f;
+    CGFloat height = section == 0 ? 46.0f : 26.0f;
     
     if ([EDDHelpers isTablet] && section == 0) {
         height = 64.5f;
@@ -271,102 +285,102 @@ enum {
 }
 
 - (NSInteger)siteCount {
-	return [[[EDDSettingsHelper getSites] allKeys] count];
+    return [[[EDDSettingsHelper getSites] allKeys] count];
 }
 
 - (NSDictionary *)getSite:(NSInteger)index {
-	NSArray *keys = [[EDDSettingsHelper getSites] allKeys];
-	id aKey = [keys objectAtIndex:index];
-	NSDictionary *site = [[EDDSettingsHelper getSites] objectForKey:aKey];
-	return site;
+    NSArray *keys = [[EDDSettingsHelper getSites] allKeys];
+    id aKey = [keys objectAtIndex:index];
+    NSDictionary *site = [[EDDSettingsHelper getSites] objectForKey:aKey];
+    return site;
 }
 
 #pragma mark - table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-	[tableView deselectRowAtIndexPath: indexPath animated: NO];
-	
-	if (indexPath.section == 0) {
-		if ([EDDSettingsHelper isCommissionOnlySite]) {
-			switch (indexPath.row) {
-				case 0:
-					[self showMainController];
-					break;
-				case 1:
-					[self showCommissionsController];
-					break;
-				case 2:
-					[self showSetupController];
-					break;
-			}
-		} else if ([EDDSettingsHelper isStandardAndCommissionSite] || [EDDSettingsHelper isStandardAndStoreCommissionSite]) {
-			switch (indexPath.row) {
-				case 0:
-					[self showMainController];
-					break;
-				case 1:
-					[self showEarningsController];
-					break;
-				case 2:
-					[self showProductsController];
-					break;
-				case 3:
-					[self showSalesController];
+    [tableView deselectRowAtIndexPath: indexPath animated: NO];
+    
+    if (indexPath.section == 0) {
+        if ([EDDSettingsHelper isCommissionOnlySite]) {
+            switch (indexPath.row) {
+                case 0:
+                    [self showMainController];
+                    break;
+                case 1:
+                    [self showCommissionsController];
+                    break;
+                case 2:
+                    [self showSetupController];
+                    break;
+            }
+        } else if ([EDDSettingsHelper isStandardAndCommissionSite] || [EDDSettingsHelper isStandardAndStoreCommissionSite]) {
+            switch (indexPath.row) {
+                case 0:
+                    [self showMainController];
+                    break;
+                case 1:
+                    [self showEarningsController];
+                    break;
+                case 2:
+                    [self showProductsController];
+                    break;
+                case 3:
+                    [self showSalesController];
                     break;
                 case 4:
                     [self showCustomersController];
                     break;
-				case 5:
-					[self showCommissionsController];
-					break;
-				case 6:
-					[self showAboutController];
+                case 5:
+                    [self showCommissionsController];
+                    break;
+                case 6:
+                    [self showAboutController];
                     break;
                 case 7:
                     [self showSetupController];
                     break;
-			}
-		} else {
-			switch (indexPath.row) {
-				case 0:
-					[self showMainController];
-					break;
-				case 1:
-					[self showEarningsController];
-					break;
-				case 2:
-					[self showProductsController];
-					break;
-				case 3:
-					[self showSalesController];
+            }
+        } else {
+            switch (indexPath.row) {
+                case 0:
+                    [self showMainController];
+                    break;
+                case 1:
+                    [self showEarningsController];
+                    break;
+                case 2:
+                    [self showProductsController];
+                    break;
+                case 3:
+                    [self showSalesController];
                     break;
                 case 4:
                     [self showCustomersController];
                     break;
-				case 5:
-					[self showAboutController];
-					break;
-				case 6:
-					[self showSetupController];
-					break;
-			}
-		}
-	} else {
-		NSDictionary *site = [self getSite:indexPath.row];
-		[EDDSettingsHelper setCurrentSiteID:[site objectForKey:KEY_FOR_SITE_ID]];
-		[[EDDAPIClient sharedClient] reload];
-		[self.tableView reloadData];
-		
-		if ([self isShowingClass:[EDDMainViewController class]]) {
-			UINavigationController *navController = (UINavigationController *)self.slideMenuController.contentViewController;
-			navController.navigationBar.translucent = NO;
-			EDDMainViewController *main = (EDDMainViewController *)navController.visibleViewController;
-			[main reload:nil];
-			[self.slideMenuController toggleMenuAnimated:self];
-		} else {
+                case 5:
+                    [self showAboutController];
+                    break;
+                case 6:
+                    [self showSetupController];
+                    break;
+            }
+        }
+    } else {
+        NSDictionary *site = [self getSite:indexPath.row];
+        [EDDSettingsHelper setCurrentSiteID:[site objectForKey:KEY_FOR_SITE_ID]];
+        [[EDDAPIClient sharedClient] reload];
+        [self.tableView reloadData];
+        
+        if ([self isShowingClass:[EDDMainViewController class]]) {
+            UINavigationController *navController = (UINavigationController *)self.slideMenuController.contentViewController;
+            navController.navigationBar.translucent = NO;
+            EDDMainViewController *main = (EDDMainViewController *)navController.visibleViewController;
+            [main reload:nil];
+            [self.slideMenuController toggleMenuAnimated:self];
+        } else {
             [self displayViewController:[[EDDMainViewController alloc] init]];
-		}
-	}
+        }
+    }
 }
 
 - (BOOL)isShowingClass:(Class)class {
