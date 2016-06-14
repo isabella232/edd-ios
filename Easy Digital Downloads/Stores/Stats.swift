d2f975577df6590f87c1480b94e0e51ca24633cb
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-06-14 09:16:50 +0100

    Move Stats from Core Data to use Structs and NSKeyedValueArchiver. Core data is not necessary for the stats.

---
+++ Easy Digital Downloads/Stores/Stats.swift
@@ -0,0 +1,67 @@
+//
+//  Stats.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 14/06/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Stats {
+    
+    let sales: NSDictionary
+    let earnings: NSDictionary
+    let updatedAt: NSDate
+    
+    static func encode(stats: Stats) {
+        let statsClassObject = StatsStorageAgent(stats: stats)
+        
+        NSKeyedArchiver.archiveRootObject(statsClassObject, toFile: StatsStorageAgent.path())
+    }
+    
+    static func decode() -> Stats? {
+        let statsClassObject = NSKeyedUnarchiver.unarchiveObjectWithFile(StatsStorageAgent.path()) as? Stats
+        
+        return statsClassObject?.stats
+    }
+    
+}
+
+extension Stats {
+
+    class StatsStorageAgent: NSObject, NSCoding {
+        
+        var stats: Stats?
+        
+        init(stats: Stats) {
+            self.stats = stats
+            super.init()
+        }
+        
+        class func path() -> String {
+            let documentsPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).first
+            let path = documentsPath?.stringByAppendingString("/Stats")
+            return path!
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            guard let sales = aDecoder.decodeObjectForKey("sales") as? NSDictionary else { stats = nil; super.init(); return nil }
+            guard let earnings = aDecoder.decodeObjectForKey("earnings") as? NSDictionary else { stats = nil; super.init(); return nil }
+            guard let updatedAt = aDecoder.decodeObjectForKey("updatedAt") as? NSDate else { stats = nil; super.init(); return nil }
+            
+            stats = Stats(sales: sales, earnings: earnings, updatedAt: updatedAt)
+            
+            super.init()
+        }
+        
+        func encodeWithCoder(aCoder: NSCoder) {
+            aCoder.encodeObject(stats!.sales, forKey: "sales")
+            aCoder.encodeObject(stats!.earnings, forKey: "earnings")
+            aCoder.encodeObject(stats!.updatedAt, forKey: "updatedAt")
+        }
+        
+    }
+
+}
