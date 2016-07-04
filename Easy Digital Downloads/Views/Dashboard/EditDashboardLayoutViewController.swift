316976241493a7e8fc203a677d1aa444b83e04e8
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-07-04 13:07:04 +0100

    Add EditDashboardLayoutViewController

---
+++ Easy Digital Downloads/Views/Dashboard/EditDashboardLayoutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditDashboardLayoutViewController.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 04/07/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import UIKit
+
+class EditDashboardLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
