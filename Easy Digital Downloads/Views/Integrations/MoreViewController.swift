8ce1a69942c5d64be3a6a3c5bb0eea61cedebf10
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-19 13:16:27 +0100

    Add base view controller which will show more options

--- Easy Digital Downloads/Views/Integrations/MoreViewController.swift
+++ Easy Digital Downloads/Views/Integrations/MoreViewController.swift
@@ -7,29 +7,28 @@
 //
 
 import UIKit
+import CoreData
 
-class MoreViewController: UIViewController {
+class MoreViewController: SiteTableViewController, ManagedObjectContextSettable {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var managedObjectContext: NSManagedObjectContext!
+    
+    var site: Site?
+    
+    init(site: Site) {
+        super.init(style: .Plain)
+        
+        self.site = site
+        
+        title = NSLocalizedString("More", comment: "Dashboard title")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = estimatedHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
-    */
 
 }
