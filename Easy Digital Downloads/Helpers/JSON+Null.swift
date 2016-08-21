1cd66c6943f66a5f647eff7f169f0a63913143b9
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-21 18:47:40 +0100

    Add new KeyCodable protocol to store all the entity keys

---
+++ Easy Digital Downloads/Helpers/JSON+Null.swift
@@ -0,0 +1,9 @@
+//
+//  JSON+Null.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 21/08/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import Foundation
