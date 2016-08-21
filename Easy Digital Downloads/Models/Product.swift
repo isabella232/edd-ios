e1f77c7573704647017fc36f6c91456e4ecc67f0
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-21 09:36:03 +0100

    Add attributes and relationships for Product data model

--- Easy Digital Downloads/Models/Product.swift
+++ Easy Digital Downloads/Models/Product.swift
@@ -7,7 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
-class Product: ManagedObject {
+public final class Product: ManagedObject {
 
+    // Attributes
+    @NSManaged public private(set) var content: String
+    @NSManaged public private(set) var createdAt: NSDate
+    @NSManaged public private(set) var createdDate: NSDate
+    @NSManaged public private(set) var files: NSDictionary
+    @NSManaged public private(set) var hasVariablePricing: NSNumber
+    @NSManaged public private(set) var link: String
+    @NSManaged public private(set) var modifiedDate: NSDate
+    @NSManaged public private(set) var notes: String
+    @NSManaged public private(set) var pid: Int16
+    @NSManaged public private(set) var pricing: NSDictionary
+    @NSManaged public private(set) var stats: NSDictionary
+    @NSManaged public private(set) var status: String
+    @NSManaged public private(set) var thumbnail: String
+    @NSManaged public private(set) var title: String
+    
+    // Relationships
+    @NSManaged private(set) var subscriptions: Set<Subscription>
+    @NSManaged public private(set) var site: Site
+    @NSManaged private(set) var sales: Set<Sale>
+    
 }
