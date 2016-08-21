dd09b7017d5012a6418ac9871b183f6bd3a5a68f
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-21 09:35:43 +0100

    Add attributes and relationships for Customer data model

--- Easy Digital Downloads/Models/Customer.swift
+++ Easy Digital Downloads/Models/Customer.swift
@@ -7,7 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
-class Customer: ManagedObject {
+public final class Customer: ManagedObject {
 
+    // Attributes
+    @NSManaged public private(set) var createdAt: NSDate
+    @NSManaged public private(set) var displayName: String
+    @NSManaged public private(set) var email: String
+    @NSManaged public private(set) var firstName: String
+    @NSManaged public private(set) var lastName: String
+    @NSManaged public private(set) var totalDownloads: Int16
+    @NSManaged public private(set) var totalPurchases: Int16
+    @NSManaged public private(set) var totalSpent: Double
+    @NSManaged public private(set) var uid: String
+    @NSManaged public private(set) var username: String
+    
+    // Relationships
+    @NSManaged private(set) var subscriptions: Set<Subscription>
+    @NSManaged private(set) var sales: Set<Sale>
+    @NSManaged public private(set) var site: Site
+    
 }
