fe8a8a33708ef1a57b578f72037993d5e3e30be0
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-21 09:35:55 +0100

    Add attributes and relationships for Discount data model

--- Easy Digital Downloads/Models/Discount.swift
+++ Easy Digital Downloads/Models/Discount.swift
@@ -7,7 +7,30 @@
 //
 
 import UIKit
+import CoreData
 
-class Discount: ManagedObject {
+public final class Discount: ManagedObject {
 
+    // Attributes
+    @NSManaged public private(set) var amount: Double
+    @NSManaged public private(set) var code: String
+    @NSManaged public private(set) var createdAt: NSDate
+    @NSManaged public private(set) var did: Int16
+    @NSManaged public private(set) var expiryDate: NSDate
+    @NSManaged public private(set) var globalDiscount: NSNumber
+    @NSManaged public private(set) var maxUses: Int16
+    @NSManaged public private(set) var minPrice: Double
+    @NSManaged public private(set) var name: String
+    @NSManaged public private(set) var productRequirements: NSArray
+    @NSManaged public private(set) var requirementConditions: String
+    @NSManaged public private(set) var singleUse: NSNumber
+    @NSManaged public private(set) var startDate: NSDate
+    @NSManaged public private(set) var status: String
+    @NSManaged public private(set) var type: String
+    @NSManaged public private(set) var uses: Int16
+    
+    // Relationships
+    @NSManaged public private(set) var site: Site
+    @NSManaged private(set) var sales: Set<Sale>
+    
 }
