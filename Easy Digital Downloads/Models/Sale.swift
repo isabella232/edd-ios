c8fe59ae35f1d739ab9cda2f00b3107292d30ca4
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-21 09:39:16 +0100

    Add attributes and relationships for Sale data model

--- Easy Digital Downloads/Models/Sale.swift
+++ Easy Digital Downloads/Models/Sale.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import CoreData
 
-class Sale: ManagedObject {
+public final class Sale: ManagedObject {
 
+    // Attributes
+    @NSManaged public private(set) var createdAt: NSDate
+    @NSManaged public private(set) var date: NSDate
+    @NSManaged public private(set) var email: String
+    @NSManaged public private(set) var fees: NSDictionary
+    @NSManaged public private(set) var gateway: String
+    @NSManaged public private(set) var key: String
+    @NSManaged public private(set) var sid: Int16
+    @NSManaged public private(set) var subtotal: Double
+    @NSManaged public private(set) var tax: Double
+    @NSManaged public private(set) var total: Double
+    @NSManaged public private(set) var transactionId: String
+    
+    // Relationships
+    @NSManaged public private(set) var site: Site
+    @NSManaged private(set) var products: Set<Product>
+    @NSManaged private(set) var discounts: Set<Discount>
+    @NSManaged public private(set) var customer: Customer
+    
 }
