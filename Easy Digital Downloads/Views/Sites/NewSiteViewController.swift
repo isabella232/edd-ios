93ba793afcf61b9d554a9e8235d93b5a7637d9b0
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-22 22:12:13 +0100

    Create base NewSiteViewController

---
+++ Easy Digital Downloads/Views/Sites/NewSiteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewSiteViewController.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 22/08/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import UIKit
+
+class NewSiteViewController: UIViewController {
+
+    
+
+}
