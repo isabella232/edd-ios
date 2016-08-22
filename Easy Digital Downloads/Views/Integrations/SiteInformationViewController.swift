d4e25f473aadd3d536834b6fe8e2edad5f2835a2
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-22 22:07:40 +0100

    Add Today Widget target to the project. #44

---
+++ Easy Digital Downloads/Views/Integrations/SiteInformationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SiteInformationViewController.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 22/08/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import Cocoa
+
+class SiteInformationViewController: UITableViewCell {
+
+}
