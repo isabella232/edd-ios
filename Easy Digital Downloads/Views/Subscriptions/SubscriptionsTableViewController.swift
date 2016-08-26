c016db01f09e784f1845ad5cc34f1a16c30255e2
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-26 09:34:00 +0100

    Create SubscriptionTableViewController. #50

---
+++ Easy Digital Downloads/Views/Subscriptions/SubscriptionsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SubscriptionsTableViewController.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 26/08/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SubscriptionsTableViewController: SiteTableViewController {
+
+    
+    
+}
