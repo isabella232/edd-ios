084821339d74f49f80ba7b193440a49a6711b8d9
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-08-29 17:03:04 +0100

    Create initial Search View Controller #39

---
+++ Easy Digital Downloads/Views/Integrations/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Easy Digital Downloads
+//
+//  Created by Sunny Ratilal on 29/08/2016.
+//  Copyright © 2016 Easy Digital Downloads. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import SwiftyJSON
+
+class SearchViewController: SiteTableViewController {
+
+    
+    
+}
