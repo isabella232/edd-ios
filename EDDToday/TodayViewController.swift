d5776f5e267e1c30bc6d771f4acdb0af825c8b2b
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-09-12 23:07:26 +0100

    Build today extension UI. #44

--- EDDToday/TodayViewController.swift
+++ EDDToday/TodayViewController.swift
@@ -8,26 +8,25 @@
 
 import UIKit
 import NotificationCenter
+import SwiftyJSON
+import CoreData
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet weak var salesLabel: UILabel!
+    @IBOutlet weak var earningsLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-
         completionHandler(NCUpdateResult.NewData)
     }
     
