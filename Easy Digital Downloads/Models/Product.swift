55f29bb292957bf0dea1cbed98c541eb6ebd6ae0
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-09-18 15:49:11 +0100

    Add persistence and search functions for Product entity

--- Easy Digital Downloads/Models/Product.swift
+++ Easy Digital Downloads/Models/Product.swift
@@ -20,7 +20,7 @@ public final class Product: ManagedObject {
     @NSManaged public private(set) var link: String
     @NSManaged public private(set) var modifiedDate: NSDate
     @NSManaged public private(set) var notes: String
-    @NSManaged public private(set) var pid: Int16
+    @NSManaged public private(set) var pid: Int64
     @NSManaged public private(set) var pricing: NSDictionary
     @NSManaged public private(set) var stats: NSDictionary
     @NSManaged public private(set) var status: String
@@ -35,6 +35,46 @@ public final class Product: ManagedObject {
     public override func awakeFromInsert() {
         super.awakeFromInsert()
         createdAt = NSDate()
+    }
+    
+    public static func predicateForId(productId: Int64) -> NSPredicate {
+        return NSPredicate(format: "%K == %lld", Product.Keys.ID.rawValue, productId)
+    }
+    
+    public static func insertIntoContext(moc: NSManagedObjectContext, content: String, createdDate: NSDate, files: NSDictionary, hasVariablePricing: NSNumber, link: String, modifiedDate: NSDate, notes: String, pid: Int64, pricing: NSDictionary, stats: NSDictionary, status: String, thumbnail: String, title: String) -> Product {
+        let product: Product = moc.insertObject()
+        product.content = content
+        product.createdDate = createdDate
+        product.files = files
+        product.hasVariablePricing = hasVariablePricing
+        product.link = link
+        product.modifiedDate = modifiedDate
+        product.notes = notes
+        product.pid = pid
+        product.pricing = pricing
+        product.stats = stats
+        product.status = status
+        product.thumbnail = thumbnail
+        product.title = title
+        product.site = Site.fetchRecordForActiveSite(inContext: moc)
+        
+        return product
+    }
+    
+    public static func productForId(productId: Int64) -> Product? {
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedObjectContext = appDelegate.managedObjectContext
+        
+        let product = Product.fetchInContext(managedObjectContext) { (request) in
+            request.predicate = self.predicateForId(productId)
+            request.fetchLimit = 1
+        }
+        
+        if product.count > 0 {
+            return product[0]
+        } else {
+            return nil
+        }
     }
     
 }
