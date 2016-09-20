71ab9eff878e9f29da682973d1299070ec067a5c
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-09-20 15:07:24 +0100

    Add base VC for Sales detail

--- Easy Digital Downloads/Views/Sales/SalesDetailViewController.swift
+++ Easy Digital Downloads/Views/Sales/SalesDetailViewController.swift
@@ -12,6 +12,45 @@ import SwiftyJSON
 
 class SalesDetailViewController: SiteTableViewController {
 
+    private enum CellType {
+        case Meta
+        case Product
+        case Payment
+        case Customer
+    }
     
+    private var cells = [CellType]()
+    
+    var site: Site?
+    var sale: Sale?
+    
+    init(sale: Sale) {
+        super.init(style: .Plain)
+        
+        self.site = Site.activeSite()
+        self.sale = sale
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 120.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.separatorStyle = .None
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: Table View Data Source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    // MARK: Table View Delegate
 
 }
