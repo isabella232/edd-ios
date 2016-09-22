e5d1e8df731a9dea3e684332f0863f8c658799f5
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-09-22 23:56:49 +0100

    Create a communication link between Subscriptions view controller and Core Data #50

--- Easy Digital Downloads/Views/Subscriptions/SubscriptionsViewController.swift
+++ Easy Digital Downloads/Views/Subscriptions/SubscriptionsViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 import CoreData
+import SwiftyJSON
+
+private let sharedDateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierISO8601)
+    formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return formatter
+}()
 
 class SubscriptionsViewController: SiteTableViewController, ManagedObjectContextSettable {
 
     var managedObjectContext: NSManagedObjectContext!
     
     var site: Site?
+    var subscriptions: [JSON]?
+    
+    var hasMoreSubscriptions: Bool = true {
+        didSet {
+            if (!hasMoreSubscriptions) {
+                activityIndicatorView.stopAnimating()
+            } else {
+                activityIndicatorView.startAnimating()
+            }
+        }
+    }
+    
+    var lastDownloadedPage = NSUserDefaults.standardUserDefaults().integerForKey("\(Site.activeSite().uid)-SalesPage") ?? 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        super.leftBarButtonItem = true
+        
+        setupInfiniteScrollView()
+        setupTableView()
     }
     
     init(site: Site) {
@@ -27,6 +53,7 @@ class SubscriptionsViewController: SiteTableViewController, ManagedObjectContext
         self.site = site
         
         title = NSLocalizedString("Subscriptions", comment: "Subscriptions title")
+
         tableView.delegate = self
         tableView.dataSource = self
         tableView.estimatedRowHeight = estimatedHeight
@@ -37,9 +64,130 @@ class SubscriptionsViewController: SiteTableViewController, ManagedObjectContext
         super.init(coder: aDecoder)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        networkOperations()
+    }
+    
+    private func networkOperations() {
+        sales = [JSON]()
+        
+        EDDAPIWrapper.sharedInstance.requestSubscriptions([ : ], success: { (json) in
+            if let items = json["subscriptions"].array {
+                self.subscriptions = items
+                self.updateLastDownloadedPage()
+                self.requestNextPage()
+            }
+            
+        }) { (error) in
+            fatalError()
+        }
+    }
+    
+    private func requestNextPage() {
+        EDDAPIWrapper.sharedInstance.requestSubscriptions([ "page": lastDownloadedPage ], success: { (json) in
+            if let items = json["subscriptions"].array {
+                if items.count == 20 {
+                    self.hasMoreSubscriptions = true
+                } else {
+                    self.hasMoreSubscriptions = false
+                }
+                for item in items {
+                    self.subscriptions?.append(item)
+                }
+                self.updateLastDownloadedPage()
+            }
+            self.persistSubscriptions()
+        }) { (error) in
+            fatalError()
+        }
+    }
+    
+    private func updateLastDownloadedPage() {
+        self.lastDownloadedPage = self.lastDownloadedPage + 1;
+        NSUserDefaults.standardUserDefaults().setInteger(lastDownloadedPage, forKey: "\(Site.activeSite().uid)-SalesPage")
+    }
+
+    private func persistSubscriptions() {
+        
+    }
+
+    // MARK: Scroll View Delegate
+    
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        let actualPosition: CGFloat = scrollView.contentOffset.y
+        let contentHeight: CGFloat = scrollView.contentSize.height - tableView.frame.size.height;
+        
+        if actualPosition >= contentHeight {
+            self.requestNextPage()
+        }
+    }
+    
+    // MARK: Table View Delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        guard let sale = dataSource.selectedObject else {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            return
+        }
+        
+        
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    // MARK: Private
+    
+    private typealias Data = FetchedResultsDataProvider<SubscriptionsViewController>
+    private var dataSource: TableViewDataSource<SubscriptionsViewController, Data, SubscriptionsTableViewCell>!
+    
+    private func setupTableView() {
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.registerClass(SubscriptionsTableViewCell.self, forCellReuseIdentifier: "SubscriptionCell")
+        setupDataSource()
+    }
+    
+    private func setupDataSource() {
+        let request = Subscription.defaultFetchRequest()
+        let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        let dataProvider = FetchedResultsDataProvider(fetchedResultsController: frc, delegate: self)
+        dataSource = TableViewDataSource(tableView: tableView, dataProvider: dataProvider, delegate: self)
+    }
+
+}
+
+extension SubscriptionsViewController: DataProviderDelegate {
+    
+    func dataProviderDidUpdate(updates: [DataProviderUpdate<Subscription>]?) {
+        dataSource.processUpdates(updates)
+    }
+    
+}
+
+extension SubscriptionsViewController: DataSourceDelegate {
+    
+    func cellIdentifierForObject(object: Subscription) -> String {
+        return "SubscriptionCell"
+    }
+    
+}
+
+extension SubscriptionsViewController : InfiniteScrollingTableView {
+    
+    func setupInfiniteScrollView() {
+        let bounds = UIScreen.mainScreen().bounds
+        let width = bounds.size.width
+        
+        let footerView = UIView(frame: CGRectMake(0, 0, width, 44))
+        footerView.backgroundColor = .clearColor()
+        
+        activityIndicatorView.startAnimating()
+        
+        footerView.addSubview(activityIndicatorView)
+        
+        tableView.tableFooterView = footerView
     }
     
 }
