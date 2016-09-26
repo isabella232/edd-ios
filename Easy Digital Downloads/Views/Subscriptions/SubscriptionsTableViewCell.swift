15bdcbe19d4ce265d3c7489d524fd7d583b11f9c
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-09-26 01:07:02 +0100

    UI for Subscriptions cell on main Subscription view controller. #50

--- Easy Digital Downloads/Views/Subscriptions/SubscriptionsTableViewCell.swift
+++ Easy Digital Downloads/Views/Subscriptions/SubscriptionsTableViewCell.swift
@@ -7,17 +7,92 @@
 //
 
 import UIKit
+import SwiftyJSON
+
+private let sharedDateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierISO8601)
+    formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+    formatter.dateFormat = "EEE dd MMM yyyy HH:mm"
+    return formatter
+}()
 
 class SubscriptionsTableViewCell: UITableViewCell {
 
+    private var hasSetupConstraints = false
     
+    lazy var containerStackView: UIStackView! = {
+        let stack = UIStackView()
+        stack.axis = .Vertical
+        stack.distribution = .Fill
+        stack.alignment = .Fill
+        stack.spacing = 3.0
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.setContentCompressionResistancePriority(UILayoutPriorityRequired, forAxis: .Vertical)
+        return stack
+    }()
+    
+    let nameLabel: UILabel = UILabel(frame: CGRectZero)
+    let dateLabel: UILabel = UILabel(frame: CGRectZero)
+    let disclosureImageView: UIImageView = UIImageView(image: UIImage(named: "DisclosureIndicator"))
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        layer.shouldRasterize = true
+        layer.rasterizationScale = UIScreen.mainScreen().scale
+        layer.opaque = true
+        opaque = true
+        
+        backgroundColor = .clearColor()
+        contentView.backgroundColor = .clearColor()
+        
+        nameLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        nameLabel.textColor = .EDDBlueColor()
+        
+        dateLabel.textColor = .EDDBlackColor()
+        dateLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        
+        layout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: Private
+    
+    private func layout() {
+        containerStackView.addArrangedSubview(nameLabel)
+        containerStackView.addArrangedSubview(dateLabel)
+        
+        contentView.addSubview(containerStackView)
+        
+        disclosureImageView.translatesAutoresizingMaskIntoConstraints = false
+        disclosureImageView.sizeToFit()
+        contentView.addSubview(disclosureImageView)
+        
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(NSLayoutConstraint(item: disclosureImageView, attribute: .Trailing, relatedBy: .Equal, toItem: contentView, attribute: .Trailing, multiplier: CGFloat(1), constant: -15))
+        constraints.append(NSLayoutConstraint(item: disclosureImageView, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: CGFloat(1), constant: CGFloat(0)))
+        constraints.append(containerStackView.topAnchor.constraintEqualToAnchor(contentView.topAnchor, constant: 15))
+        constraints.append(containerStackView.bottomAnchor.constraintEqualToAnchor(contentView.bottomAnchor, constant: -15))
+        constraints.append(containerStackView.leadingAnchor.constraintEqualToAnchor(contentView.leadingAnchor, constant: 15))
+        constraints.append(containerStackView.trailingAnchor.constraintEqualToAnchor(contentView.trailingAnchor, constant: -15))
+        
+        NSLayoutConstraint.activateConstraints(constraints)
+    }
 
 }
 
 extension SubscriptionsTableViewCell: ConfigurableCell {
     
     func configureForObject(object: Subscription) {
-        
+        let customer: [String: AnyObject] = object.customer
+        let name = customer["name"]!
+        nameLabel.text = name as? String
+        dateLabel.text = sharedDateFormatter.stringFromDate(object.created)
     }
     
 }
