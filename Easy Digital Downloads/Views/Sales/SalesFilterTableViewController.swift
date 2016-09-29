b409168963ba1882cd008fcbebaf212435fab66a
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-09-30 00:38:48 +0100

    Replace Done button with Close. #71

--- Easy Digital Downloads/Views/Sales/SalesFilterTableViewController.swift
+++ Easy Digital Downloads/Views/Sales/SalesFilterTableViewController.swift
@@ -8,88 +8,69 @@
 
 import UIKit
 
-class SalesFilterTableViewController: UITableViewController {
+class SalesFilterTableViewController: UIViewController {
 
+    private var tableView: UITableView!
+    private var navigationBar: UINavigationBar!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        navigationBar = UINavigationBar(frame: CGRectMake(0, 0, view.frame.width, 64))
+        navigationBar.translucent = false
+        navigationBar.barStyle = .Black
+        
+        tableView = UITableView(frame: CGRectMake(0, navigationBar.frame.height, view.frame.width, view.frame.height - navigationBar.frame.height) ,style: .Plain);
+        tableView.scrollEnabled = true
+        tableView.bounces = true
+        tableView.showsVerticalScrollIndicator = true
+        tableView.userInteractionEnabled = true
+        tableView.backgroundColor = .clearColor()
+        tableView.separatorColor = UIColor.separatorColor()
+        tableView.tableFooterView = UIView()
+//        tableView.dataSource = self
+//        tableView.delegate = self
+        
+        let navigationItem = UINavigationItem(title: NSLocalizedString("Filter Sales", comment: ""))
+        let closeButton = UIBarButtonItem(title: NSLocalizedString("Close", comment: ""), style: .Plain, target: self, action: #selector(SalesFilterTableViewController.closeButtonPressed))
+        navigationItem.leftBarButtonItem = closeButton
+        navigationBar.items = [navigationItem]
+        
+        title = NSLocalizedString("Filter Sales", comment: "")
+        
+        view.addSubview(tableView)
+        view.addSubview(navigationBar)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func closeButtonPressed() {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    func addButtonPressed() {
+        let newSiteViewController = NewSiteViewController()
+        newSiteViewController.managedObjectContext = AppDelegate.sharedInstance.managedObjectContext
+        newSiteViewController.view.backgroundColor = .clearColor()
+        newSiteViewController.modalPresentationStyle = .OverFullScreen
+        newSiteViewController.modalPresentationCapturesStatusBarAppearance = true
+        presentViewController(newSiteViewController, animated: true, completion: nil)
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return false
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // MARK: Table View Delegate
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
