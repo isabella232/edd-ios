d9369d6d30e34423539ccd74e48d00dbc469d448
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-10-15 02:27:49 +0100

    View controller title label for customer view controller

--- Easy Digital Downloads/Views/Customers/CustomerOfflineViewController.swift
+++ Easy Digital Downloads/Views/Customers/CustomerOfflineViewController.swift
@@ -58,15 +58,14 @@ class CustomerOfflineViewController: SiteTableViewController {
                 self.customer = Customer.objectForData(AppDelegate.sharedInstance.managedObjectContext, displayName: item["info"]["display_name"].stringValue, email: item["info"]["email"].stringValue, firstName: item["info"]["first_name"].stringValue, lastName: item["info"]["last_name"].stringValue, totalDownloads: item["stats"]["total_downloads"].int64Value, totalPurchases: item["stats"]["total_purchases"].int64Value, totalSpent: item["stats"]["total_spent"].doubleValue, uid: item["info"]["user_id"].int64Value, username: item["username"].stringValue, dateCreated: sharedDateFormatter.dateFromString(item["info"]["date_created"].stringValue)!)
                 
                 dispatch_async(dispatch_get_main_queue(), {
-                    self.title = item["info"]["display_name"].stringValue
+                    let titleLabel = ViewControllerTitleLabel()
+                    titleLabel.setTitle(item["info"]["display_name"].stringValue)
+                    self.navigationItem.titleView = titleLabel
+                    
                     self.tableView.reloadData()
                     self.loadingView.removeFromSuperview()
                 })
             }
-            
-            dispatch_async(dispatch_get_main_queue(), {
-                self.tableView.reloadData()
-            })
         })
         
         self.site = Site.activeSite()
