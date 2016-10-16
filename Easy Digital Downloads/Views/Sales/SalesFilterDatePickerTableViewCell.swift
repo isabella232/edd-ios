202e55521254e72aad678f6cfe9960c791574815
Sunny Ratilal <sunnyratilal5@gmail.com>
2016-10-16 17:19:19 +0100

    Add date pickers for Sales filtering #71

--- Easy Digital Downloads/Views/Sales/SalesFilterDatePickerTableViewCell.swift
+++ Easy Digital Downloads/Views/Sales/SalesFilterDatePickerTableViewCell.swift
@@ -8,8 +8,58 @@
 
 import UIKit
 
-class SalesFilterDatePickerTableViewCell: UITableViewCell {
+protocol UpdateDateCellDelegate {
+    func sendDate(date: NSDate, tag: Int)
+}
 
+class SalesFilterDatePickerTableViewCell: UITableViewCell, UIPickerViewDelegate {
+
+    var delegate: UpdateDateCellDelegate?
     
+    var datePicker: UIDatePicker!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        layer.shouldRasterize = true
+        layer.rasterizationScale = UIScreen.mainScreen().scale
+        layer.opaque = true
+        opaque = true
+        
+        datePicker = UIDatePicker(frame: CGRectZero)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.backgroundColor = .clearColor()
+        datePicker.datePickerMode = .Date
+        datePicker.addTarget(self, action: #selector(SalesFilterDatePickerTableViewCell.onDidChangeDate(_:)), forControlEvents: .ValueChanged)
+        
+        backgroundColor = .clearColor()
+        contentView.backgroundColor = .clearColor()
+        
+        layout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func layout() {
+        contentView.addSubview(datePicker)
+        
+        var constraints = [NSLayoutConstraint]()
+
+        constraints.append(datePicker.topAnchor.constraintEqualToAnchor(contentView.topAnchor, constant: 0))
+        constraints.append(datePicker.bottomAnchor.constraintEqualToAnchor(contentView.bottomAnchor, constant: 0))
+        constraints.append(NSLayoutConstraint(item: datePicker, attribute: .Leading, relatedBy: .Equal, toItem: contentView, attribute: .Leading, multiplier: CGFloat(1), constant: 0))
+        constraints.append(NSLayoutConstraint(item: datePicker, attribute: .Trailing, relatedBy: .Equal, toItem: contentView, attribute: .Trailing, multiplier: CGFloat(1), constant: 0))
+        constraints.append(NSLayoutConstraint(item: datePicker, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: CGFloat(1), constant: CGFloat(0)))
+        
+        NSLayoutConstraint.activateConstraints(constraints)
+    }
+    
+    internal func onDidChangeDate(sender: UIDatePicker) {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.delegate?.sendDate(sender.date, tag: self.tag)
+        })
+    }
 
 }
--- Easy Digital Downloads/Views/Sales/SalesFilterTableViewController.swift
+++ Easy Digital Downloads/Views/Sales/SalesFilterTableViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class SalesFilterTableViewController: UIViewController {
+private let sharedDateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .ShortStyle
+    return formatter
+}()
+
+
+class SalesFilterTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UpdateDateCellDelegate {
 
     private var tableView: UITableView!
     private var navigationBar: UINavigationBar!
+    
+    private var startDateIndexPath: NSIndexPath!
+    private var endDateIndexPath: NSIndexPath!
+    
+    private var dates: [String] = ["", ""]
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -32,15 +44,24 @@ class SalesFilterTableViewController: UIViewController {
         tableView.backgroundColor = .clearColor()
         tableView.separatorColor = UIColor.separatorColor()
         tableView.tableFooterView = UIView()
-//        tableView.dataSource = self
-//        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.estimatedRowHeight = 200.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        tableView.registerClass(SalesFilterDatePickerTableViewCell.self, forCellReuseIdentifier: "SalesFilterDatePickerTableViewCell")
+        
+        title = NSLocalizedString("Filter Sales", comment: "")
+
         
         let navigationItem = UINavigationItem(title: NSLocalizedString("Filter Sales", comment: ""))
         let closeButton = UIBarButtonItem(title: NSLocalizedString("Close", comment: ""), style: .Plain, target: self, action: #selector(SalesFilterTableViewController.closeButtonPressed))
         navigationItem.leftBarButtonItem = closeButton
-        navigationBar.items = [navigationItem]
         
-        title = NSLocalizedString("Filter Sales", comment: "")
+        let titleLabel = ViewControllerTitleLabel()
+        titleLabel.setTitle(NSLocalizedString("Filter Sales", comment: "Sales title"))
+        navigationItem.titleView = titleLabel
+        navigationBar.items = [navigationItem]
         
         view.addSubview(tableView)
         view.addSubview(navigationBar)
@@ -48,15 +69,6 @@ class SalesFilterTableViewController: UIViewController {
     
     func closeButtonPressed() {
         dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func addButtonPressed() {
-        let newSiteViewController = NewSiteViewController()
-        newSiteViewController.managedObjectContext = AppDelegate.sharedInstance.managedObjectContext
-        newSiteViewController.view.backgroundColor = .clearColor()
-        newSiteViewController.modalPresentationStyle = .OverFullScreen
-        newSiteViewController.modalPresentationCapturesStatusBarAppearance = true
-        presentViewController(newSiteViewController, animated: true, completion: nil)
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
@@ -70,7 +82,117 @@ class SalesFilterTableViewController: UIViewController {
     // MARK: Table View Delegate
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+            case 0:
+                return 1
+            case 1:
+                return 4
+            case 2:
+                return 1
+            default:
+                return 1
+        }
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return NSLocalizedString("Date Filter", comment: "")
+        }
+        
+        return nil
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view: UIView = UIView(frame: CGRectMake(0, 0, tableView.bounds.size.width, 40))
+        view.backgroundColor = .EDDGreyColor()
+        view.userInteractionEnabled = false
+        view.tag = section
+        
+        let label = UILabel(frame: CGRectMake(15, 25, tableView.bounds.size.width - 10, 20))
+        label.text = self.tableView(tableView, titleForHeaderInSection: section)?.uppercaseString
+        label.textColor = .EDDBlackColor()
+        label.font = UIFont.systemFontOfSize(14, weight: UIFontWeightLight)
+        label.textAlignment = .Left
+        
+        view.addSubview(label)
+        
+        return view
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    // MARK: Table View Data Source
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell: UITableViewCell = UITableViewCell()
+            
+            cell.selectionStyle = .None
+            cell.backgroundColor = .clearColor()
+            cell.textLabel?.text = "Set a start and end date to view sales between a certain time period"
+            cell.textLabel?.textColor = .EDDBlackColor()
+            cell.textLabel?.lineBreakMode = .ByWordWrapping
+            cell.textLabel?.numberOfLines = 0
+            
+            return cell
+        }
+        
+        if indexPath.section == 1 && (indexPath.row == 1 || indexPath.row == 3) {
+            let cell: SalesFilterDatePickerTableViewCell = tableView.dequeueReusableCellWithIdentifier("SalesFilterDatePickerTableViewCell", forIndexPath: indexPath) as! SalesFilterDatePickerTableViewCell
+        
+            cell.tag = indexPath.row
+            cell.delegate = self
+            
+            return cell
+        } else if indexPath.section == 1 {
+            let cell: UITableViewCell = UITableViewCell(style: .Value1, reuseIdentifier: "StaticCell")
+            cell.backgroundColor = .whiteColor()
+            cell.textLabel?.textColor = .EDDBlackColor()
+            cell.selectionStyle = .None
+            
+            if indexPath.row == 0 {
+                cell.textLabel?.text = NSLocalizedString("Start Date", comment: "")
+                cell.detailTextLabel?.text = dates[0]
+            }
+            
+            if indexPath.row == 2 {
+                cell.textLabel?.text = NSLocalizedString("End Date", comment: "")
+                cell.detailTextLabel?.text = dates[1]
+            }
+            
+            return cell
+        }
+        
+        let cell: UITableViewCell = UITableViewCell()
+        return cell
+    }
+    
+    // MARK: UpdateDataCellDelegate
+    
+    func sendDate(date: NSDate, tag: Int) {
+        // Start Date
+        if tag == 1 {
+            let indexPath = NSIndexPath(forRow: 0, inSection: 1)
+            dates[0] = sharedDateFormatter.stringFromDate(date)
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            })
+        }
+        
+        // End Date
+        if tag == 3 {
+            let indexPath = NSIndexPath(forRow: 2, inSection: 1)
+            dates[1] = sharedDateFormatter.stringFromDate(date)
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            })
+        }
     }
 
 }
