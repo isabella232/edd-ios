b5011918e84ae4509ae9ccf672995561bf0df55e
Sunny Ratilal <sunny@sunnyratilal.com>
2017-06-15 20:22:33 +0100

    Switch encoding from JSON to URL

--- Easy Digital Downloads/Views/Sites/NewSiteViewController.swift
+++ Easy Digital Downloads/Views/Sites/NewSiteViewController.swift
@@ -334,7 +334,7 @@ class NewSiteViewController: UIViewController, UITextFieldDelegate, ManagedObjec
             self.connectionTest.isHidden = false
         }) 
         
-        Alamofire.request(siteURL.text! + "/edd-api/info", method: .get, parameters: ["key": apiKey.text!, "token": token.text!], encoding: JSONEncoding.default, headers: nil)
+        Alamofire.request(siteURL.text! + "/edd-api/info", method: .get, parameters: ["key": apiKey.text!, "token": token.text!], encoding: URLEncoding.default, headers: nil)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
             .responseJSON { response in
